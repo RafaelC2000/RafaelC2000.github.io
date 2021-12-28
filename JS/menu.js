7702cc8e3af593966cca1ca76371c9a0647072ad
Ernesto Cedillo <rafacrteto@outlook.com>
2021-12-27 21:31:25 -0600

    update menu links

--- JS/menu.js
+++ JS/menu.js
@@ -1,14 +1,20 @@
 $menu=document.querySelector(".menu")
 $close=document.querySelector(".close")
 $top=document.querySelector(".top")
+$nav=document.querySelector(".nav")
+
 
 $menu.addEventListener('click', ()=>{
     $top.classList.add('show')
     $close.classList.add('show')
-    console.log('hola')
 })
 
 $close.addEventListener('click', ()=>{
+    $top.classList.remove('show')
+    $close.classList.remove('show')
+})
+
+$nav.addEventListener('click', ()=>{
     $top.classList.remove('show')
     $close.classList.remove('show')
 })
